4ca62d9499d61fffe277e62be7a0c05af63c8213
Praveen Kumar K S <pravks27@in.ibm.com>
2022-12-11 13:48:23 +0530

    accenture-assets

---
+++ src/test/java/com/ibm/partner/sample/SampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.partner.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
